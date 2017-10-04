6ce83131bca4840e221709bb4fc74698af6dd7cb
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-10-04 14:08:54 +0500

    model fix and entity
    - Landstalker
    - Mesa

--- lua/autorun/simfphys_gta_sa_suvs_pickups_ligths.lua
+++ lua/autorun/simfphys_gta_sa_suvs_pickups_ligths.lua
@@ -101,3 +101,110 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "huntley", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 75, 22, -2 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 75, -22, -2 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-83,34,-3),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-83,-34,-3),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 75, 22, -2 ),material = "sprites/light_ignorez",size = 42, color = Color( 255,230,210,150)},
+		{pos = Vector( 75, -22, -2 ),material = "sprites/light_ignorez",size = 42, color = Color( 255,230,210,150)},
+		
+		
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 75, 22, -2 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 75, -22, -2 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-83,34,-3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-83,-34,-3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-83,34,-3),material = "sprites/light_ignorez",size = 50,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-83,-34,-3),material = "sprites/light_ignorez",size = 50,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-83,32,-3),material = "sprites/light_ignorez",size = 25,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-83,-32,-3),material = "sprites/light_ignorez",size = 25,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-83,34,-3),
+			Vector( 74, 33, -2 ),
+			Vector( 77, 25, -21 ),
+		},
+		Right = {
+			Vector(-83,-34,-3),
+			Vector( 74, -33, -2 ),
+			Vector( 77, -25, -21 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "Landstalker", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 72, 16, 0 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 72, -16, 0 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-68,28,1),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-68,-28,1),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 72, 16, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,210,150)},
+		{pos = Vector( 72, -16, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,210,150)},
+		
+		
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 72, 16, 0 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+		{pos = Vector( 72, -16, 0 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-70,28,1),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-70,-28,1),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-70,28,1),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-70,-28,1),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-70,28,1),material = "sprites/light_ignorez",size = 25,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-70,-28,1),material = "sprites/light_ignorez",size = 25,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-70,28,-2),
+			Vector( 69, 33, -1 ),
+			Vector( 76, 15, -8 ),
+		},
+		Right = {
+			Vector(-70,-28,-2),
+			Vector( 69, -33, -1 ),
+			Vector( 76, -15, -8 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "mesa", light_table)
+
