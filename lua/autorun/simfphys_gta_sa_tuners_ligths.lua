b08b431ea2a44927257971b34b5e5610c747cd0d
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-10-11 17:59:32 +0500

    entity
    - Sultan

--- lua/autorun/simfphys_gta_sa_tuners_ligths.lua
+++ lua/autorun/simfphys_gta_sa_tuners_ligths.lua
@@ -211,3 +211,56 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "stratum", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 92, 23, 2 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 92, -23, 2 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-86,25,7),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-86,-25,7),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 92, 23, 2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 92, -23, 2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 91, 26, 2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 91, -26, 2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 90, 29, 2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 90, -29, 2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 93, 25, 2 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+		{pos = Vector( 93, -25, 2 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-86,25,7),material = "sprites/light_ignorez",size = 30,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-86,-25,7),material = "sprites/light_ignorez",size = 30,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-86,32,6),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-86,-32,6),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-86,24,4),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-86,-24,4),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-83,34,3),
+			Vector( 85, 37, 2 ),
+		},
+		Right = {
+			Vector(-83,-34,3),
+			Vector( 85, -37, 2 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "sultan", light_table)
