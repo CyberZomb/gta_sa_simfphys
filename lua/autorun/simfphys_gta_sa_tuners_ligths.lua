808ac5154ebfba81dff3be5f04a49f50f952409c
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-10-11 15:31:29 +0500

    entity
    - Jester

--- lua/autorun/simfphys_gta_sa_tuners_ligths.lua
+++ lua/autorun/simfphys_gta_sa_tuners_ligths.lua
@@ -103,3 +103,58 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "flash", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 78, 23, 0 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 78, -23, 0 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-83,26,6),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-83,-26,6),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 89, 21, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 89, -21, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 89, 25, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 89, -25, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 89, 28, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 89, -28, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 89, 25, 0 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 89, -25, 0 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-84,19,6),material = "sprites/light_ignorez",size = 25,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-84,-19,6),material = "sprites/light_ignorez",size = 25,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-83,26,6),material = "sprites/light_ignorez",size = 25,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-83,-26,6),material = "sprites/light_ignorez",size = 25,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-83,26,6),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-83,-26,6),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-82,31,7),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-82,-31,7),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-82,31,7),
+			Vector( 85, 32, 0 ),
+		},
+		Right = {
+			Vector(-82,-31,7),
+			Vector( 85, -32, 0 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "jester", light_table)
--- lua/autorun/simfphys_gta_sa_tuners_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_tuners_spawnlist.lua
@@ -229,3 +229,119 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_flash", V )
+
+local V = {
+	Name = "Jester",
+	Model = "models/GTA_SA/tuners/jester.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Tuners",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 1500,
+		
+		EnginePos = Vector(60,0,10),
+		
+		LightsTable = "jester",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/GTA_SA/tuners/jester_wheel.mdl",
+		CustomWheelPosFL = Vector(53,35,-12),
+		CustomWheelPosFR = Vector(53,-35,-12),
+		CustomWheelPosRL = Vector(-52,35,-12),
+		CustomWheelPosRR = Vector(-52,-35,-12),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(10,0,0),
+		
+		CustomSteerAngle = 30,
+		
+		SeatOffset = Vector(-13,-16,15),
+		SeatPitch = -10,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(-1,-17,-17),
+				ang = Angle(0,-90,25)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-84,-21,-13),
+				ang = Angle(90,180,0),
+			},{
+				pos = Vector(-84,21,-13),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 6,
+		FrontConstant = 36000,
+		FrontDamping = 1300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 5,
+		RearConstant = 36000,
+		RearDamping = 1300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 10,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 45,
+		Efficiency = 1,
+		GripOffset = -2,
+		BrakePower = 34,
+		
+		IdleRPM = 800,
+		LimitRPM = 7800,
+		PeakTorque = 190,
+		PowerbandStart = 900,
+		PowerbandEnd = 7600,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-64,37,7),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 60,
+		
+		PowerBias = -1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_086/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_086/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_086/sound_001.wav",
+		Sound_HighPitch = 0.9,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_087/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.6,
+		Gears = {-0.12,0,0.1,0.17,0.25,0.34,0.45}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_jester", V )
