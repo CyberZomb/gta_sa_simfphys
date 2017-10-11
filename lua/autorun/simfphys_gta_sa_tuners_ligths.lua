c7d80649859e98ad4003d59f8d994e3ff878396c
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-10-11 17:21:12 +0500

    entity
    - Stratum

--- lua/autorun/simfphys_gta_sa_tuners_ligths.lua
+++ lua/autorun/simfphys_gta_sa_tuners_ligths.lua
@@ -158,3 +158,56 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "jester", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 97, 18, -4 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 97, -18, -4 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-94,31,0),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-94,-31,0),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 97, 18, -4 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 97, -18, -4 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 97, 22, -4 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 97, -22, -4 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 97, 25, -4 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 97, -25, -4 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 97, 22, -4 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+		{pos = Vector( 97, -22, -4 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-94,31,0),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-94,-31,0),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-94,31,0),material = "sprites/light_ignorez",size = 50,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-94,-31,0),material = "sprites/light_ignorez",size = 50,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-94,31,0),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-94,-31,0),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-91,35,-2),
+			Vector( 93, 33, -4 ),
+		},
+		Right = {
+			Vector(-91,-35,-2),
+			Vector( 93, -33, -4 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "stratum", light_table)
--- lua/autorun/simfphys_gta_sa_tuners_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_tuners_spawnlist.lua
@@ -345,3 +345,131 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_jester", V )
+
+local V = {
+	Name = "Stratum",
+	Model = "models/GTA_SA/tuners/stratum.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Tuners",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 1800,
+		
+		EnginePos = Vector(60,0,10),
+		
+		LightsTable = "stratum",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/GTA_SA/tuners/stratum_wheel.mdl",
+		CustomWheelPosFL = Vector(61,33,-18),
+		CustomWheelPosFR = Vector(61,-33,-18),
+		CustomWheelPosRL = Vector(-51,33,-18),
+		CustomWheelPosRR = Vector(-51,-33,-18),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(10,0,0),
+		
+		CustomSteerAngle = 30,
+		
+		SeatOffset = Vector(-2,-18,15),
+		SeatPitch = -4,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(10,-17,-17),
+				ang = Angle(0,-90,18)
+			},
+			{
+				pos = Vector(-23,-18,-15),
+				ang = Angle(0,-90,18)
+			},
+			{
+				pos = Vector(-23,18,-15),
+				ang = Angle(0,-90,18)
+			},
+			{
+				pos = Vector(-23,0,-15),
+				ang = Angle(0,-90,18)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-100,-17,-18),
+				ang = Angle(90,180,0),
+			},{
+				pos = Vector(-100,17,-18),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 6,
+		FrontConstant = 39000,
+		FrontDamping = 1600,
+		FrontRelativeDamping = 1600,
+		
+		RearHeight = 5,
+		RearConstant = 39000,
+		RearDamping = 1600,
+		RearRelativeDamping = 1600,
+		
+		FastSteeringAngle = 10,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 45,
+		Efficiency = 1,
+		GripOffset = -2,
+		BrakePower = 34,
+		
+		IdleRPM = 800,
+		LimitRPM = 7800,
+		PeakTorque = 200,
+		PowerbandStart = 900,
+		PowerbandEnd = 7600,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-84,-37,3),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 60,
+		
+		PowerBias = 0.9,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_080/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_080/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_080/sound_001.wav",
+		Sound_HighPitch = 0.9,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_081/sound_001.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.6,
+		Gears = {-0.12,0,0.1,0.17,0.25,0.34,0.45}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_stratum", V )
