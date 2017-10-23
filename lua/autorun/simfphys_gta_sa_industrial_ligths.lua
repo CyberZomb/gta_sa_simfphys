dc5d04419b5fdb57cc02bc80567446bcc65fa3f4
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-10-23 15:36:38 +0500

    entity of Boxville

--- lua/autorun/simfphys_gta_sa_industrial_ligths.lua
+++ lua/autorun/simfphys_gta_sa_industrial_ligths.lua
@@ -48,3 +48,54 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "benson", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 110, 32, -3 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 110, -32, -3 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-110,32,7),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-110,-32,7),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 110, 32, -3 ),material = "sprites/light_ignorez",size = 49, color = Color( 255,230,230,180)},
+		{pos = Vector( 110, -32, -3 ),material = "sprites/light_ignorez",size = 49, color = Color( 255,230,230,180)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 110, 32, -3 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 110, -32, -3 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-110,37,7),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  190)},
+		{pos = Vector(-110,-37,7),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  190)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-110,32,7),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-110,-32,7),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-110,32,13),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-110,-32,13),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-110,37,13),
+			Vector( 112, 33, 8 ),
+			Vector( 112, 30, 8 ),
+		},
+		Right = {
+			Vector(-110,-37,13),
+			Vector( 112, -33, 8 ),
+			Vector( 112, -30, 8 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "boxville", light_table)
--- lua/autorun/simfphys_gta_sa_industrial_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_industrial_spawnlist.lua
@@ -14,7 +14,7 @@ local V = {
 		LightsTable = "benson",
 		
 		CustomWheels = true,
-		CustomSuspensionTravel = 5,
+		CustomSuspensionTravel = 2,
 		
 		CustomWheelModel = "models/GTA_SA/industrial/benson_wheel.mdl",
 		CustomWheelPosFL = Vector(62,31,-24),
@@ -110,3 +110,124 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_benson", V )
+
+local V = {
+	Name = "Boxville",
+	Model = "models/GTA_SA/industrial/boxville.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Industrial",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 5500,
+		
+		EnginePos = Vector(60,0,10),
+		
+		LightsTable = "boxville",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = -6,
+		
+		CustomWheelModel = "models/GTA_SA/industrial/boxville_wheel.mdl",
+		CustomWheelPosFL = Vector(86,39,-30),
+		CustomWheelPosFR = Vector(86,-39,-30),
+		CustomWheelPosRL = Vector(-62,39,-30),
+		CustomWheelPosRR = Vector(-62,-39,-30),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(0,0,2),
+		
+		CustomSteerAngle = 30,
+		
+		SeatOffset = Vector(44,-25,39),
+		SeatPitch = 8,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(53,-25,-2),
+				ang = Angle(0,-90,8)
+			},
+			{
+				pos = Vector(-89,-25,-8),
+				ang = Angle(0,0,8)
+			},
+			{
+				pos = Vector(-69,25,-8),
+				ang = Angle(0,180,8)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-110,17,-25),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 2,
+		FrontConstant = 50000,
+		FrontDamping = 3000,
+		FrontRelativeDamping = 3000,
+		
+		RearHeight = 2,
+		RearConstant = 50000,
+		RearDamping = 3000,
+		RearRelativeDamping = 3000,
+		
+		FastSteeringAngle = 22,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 2,
+		
+		MaxGrip = 75,
+		Efficiency = 1,
+		GripOffset = -5,
+		BrakePower = 50,
+		
+		IdleRPM = 600,
+		LimitRPM = 4000,
+		PeakTorque = 280,
+		PowerbandStart = 700,
+		PowerbandEnd = 3800,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(0,41,3),
+		FuelType = FUELTYPE_DIESEL,
+		FuelTankSize = 65,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_135/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_135/sound_001.wav",
+		Sound_MidPitch = 1.2,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_135/sound_001.wav",
+		Sound_HighPitch = 1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_136/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.12,0,0.14,0.24,0.36,0.48,0.6}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_boxville", V )
