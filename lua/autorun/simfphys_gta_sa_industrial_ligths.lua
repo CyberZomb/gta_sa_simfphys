a86f8d2095a882620d2bc5b356fe9e16ddc86b7e
def-valerius <31399259+def-valerius@users.noreply.github.com>
2017-10-25 23:30:37 +0500

    entity of Yankee and industrial icons

--- lua/autorun/simfphys_gta_sa_industrial_ligths.lua
+++ lua/autorun/simfphys_gta_sa_industrial_ligths.lua
@@ -791,3 +791,58 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "tractor", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 120, 34, -2 ),
+	L_HeadLampAng = Angle(10,0,0),
+	R_HeadLampPos = Vector( 120, -34, -2 ),
+	R_HeadLampAng = Angle(10,0,0),
+	
+	L_RearLampPos = Vector(-124,41,7),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-124,-41,7),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 120, 34, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,180)},
+		{pos = Vector( 120, -34, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,180)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 120, 34, -2 ),material = "sprites/light_ignorez",size = 48, color = Color( 255,235,220,170)},
+		{pos = Vector( 120, -34, -2 ),material = "sprites/light_ignorez",size = 48, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-168,47,7),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-168,-47,7),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-168,47,12),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-168,-47,12),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-168,47,17),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-168,-47,17),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-166,34,-18),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-166,-34,-18),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-166,29,-18),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-166,-29,-18),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-166,29,-18),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-166,-29,-18),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-166,45,-23),
+			Vector( 119, 35, -4 ),
+		},
+		Right = {
+			Vector(-166,-45,-23),
+			Vector( 119, -35, -4 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "yankee", light_table)
--- lua/autorun/simfphys_gta_sa_industrial_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_industrial_spawnlist.lua
@@ -1801,3 +1801,116 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_tractor", V )
+
+local V = {
+	Name = "Yankee",
+	Model = "models/GTA_SA/industrial/yankee.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Industrial",
+	SpawnOffset = Vector(0,0,40),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 4500,
+		
+		EnginePos = Vector(100,0,10),
+		
+		LightsTable = "yankee",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 2,
+		
+		CustomWheelModel = "models/GTA_SA/industrial/yankee_wheel.mdl",
+		CustomWheelPosFL = Vector(92,38,-30),
+		CustomWheelPosFR = Vector(92,-38,-30),
+		CustomWheelPosRL = Vector(-100,44,-30),
+		CustomWheelPosRR = Vector(-100,-44,-30),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(5,0,0),
+		
+		CustomSteerAngle = 32,
+		
+		SeatOffset = Vector(43,-15,34),
+		SeatPitch = 6,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(52,-15,-6),
+				ang = Angle(0,-90,8)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-36,23,-28),
+				ang = Angle(90,135,0),
+			}
+		},
+		
+		FrontHeight = 6,
+		FrontConstant = 50000,
+		FrontDamping = 3000,
+		FrontRelativeDamping = 3000,
+		
+		RearHeight = 8,
+		RearConstant = 50000,
+		RearDamping = 3000,
+		RearRelativeDamping = 3000,
+		
+		FastSteeringAngle = 20,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 2,
+		
+		MaxGrip = 75,
+		Efficiency = 1,
+		GripOffset = -5,
+		BrakePower = 50,
+		
+		IdleRPM = 600,
+		LimitRPM = 4000,
+		PeakTorque = 300,
+		PowerbandStart = 700,
+		PowerbandEnd = 3800,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-2,-32,-20),
+		FuelType = FUELTYPE_DIESEL,
+		FuelTankSize = 69,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_135/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_135/sound_001.wav",
+		Sound_MidPitch = 1.5,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_135/sound_001.wav",
+		Sound_HighPitch = 1.1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_136/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.12,0,0.13,0.24,0.36,0.48,0.6}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_yankee", V )
