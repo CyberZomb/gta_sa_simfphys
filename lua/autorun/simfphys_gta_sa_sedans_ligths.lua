f4a6eae142ee1c30153bdd64dc469c3cdf4761df
def-valerius <31399259+def-valerius@users.noreply.github.com>
2019-02-10 14:39:38 +0500

    entities of Admiral, Elegant, Emperor

--- lua/autorun/simfphys_gta_sa_sedans_ligths.lua
+++ lua/autorun/simfphys_gta_sa_sedans_ligths.lua
@@ -1,37 +1,37 @@
 local light_table = {
-	L_HeadLampPos = Vector( 90, 23, 0 ),
+	L_HeadLampPos = Vector( 87.2, 23, -2 ),
 	L_HeadLampAng = Angle(0,0,0),
-	R_HeadLampPos = Vector( 90, -23, 0 ),
+	R_HeadLampPos = Vector( 87.2, -23, -2 ),
 	R_HeadLampAng = Angle(0,0,0),
 	
-	L_RearLampPos = Vector(-82,30,5),
+	L_RearLampPos = Vector(-82.8,19.1,2.3),
 	L_RearLampAng = Angle(0,180,0),
-	R_RearLampPos = Vector(-82,-30,5),
+	R_RearLampPos = Vector(-82.8,-19.1,2.3),
 	R_RearLampAng = Angle(0,180,0),
 	
 	Headlight_sprites = {
-		{pos = Vector( 89, 18, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
-		{pos = Vector( 89, -18, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
-		{pos = Vector( 89, 22, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
-		{pos = Vector( 89, -22, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
-		{pos = Vector( 89, 25, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
-		{pos = Vector( 89, -25, 0 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 87.5, 18, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 87.5, -18, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 87.5, 22, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 87.5, -22, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 87.5, 25, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 87.5, -25, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
 	},
 	Headlamp_sprites = {
-		{pos = Vector( 89, 23, 0 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
-		{pos = Vector( 89, -23, 0 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 87.5, 22, -2 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 87.5, -22, -2 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
 	},
 	Rearlight_sprites = {
-		{pos = Vector(-82,30,5),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
-		{pos = Vector(-82,-30,5),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-102.8,26.6,-2.3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-102.8,-26.6,-2.3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
 	},
 	Brakelight_sprites = {
-		{pos = Vector(-82,23,4),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
-		{pos = Vector(-82,-23,4),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-102.8,19.1,-2.3),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-102.8,-19.1,-2.3),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
 	},
 	Reverselight_sprites = {
-		{pos = Vector(-82,18,3),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
-		{pos = Vector(-82,-18,3),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-102.8,19.1,-2.3),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-102.8,-19.1,-2.3),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
 	},
 	
 	DelayOn = 0.1,
@@ -39,14 +39,130 @@ local light_table = {
 	
 	Turnsignal_sprites = {
 		Left = {
-			Vector(-82,23,4),
-			Vector( 85, 33, 0 ),
+			Vector(-102.8,34,-2.3),
+			Vector( 86.5, 33, -2 ),
 		},
 		Right = {
-			Vector(-82,-23,4),
-			Vector( 85, -33, 0 ),
+			Vector(-102.8,-34,-2.3),
+			Vector( 86.5, -33, -2 ),
 		},
 	}
 
 }
 list.Set( "simfphys_lights", "admiral", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 95.1, 28, -2 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 95.1, -28, -2 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-82.8,25.2,2.2),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-82.8,-25.2,2.2),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 95.1, 21, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 95.1, -21, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 95.1, 28, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 95.1, -28, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 95.1, 33, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 95.1, -33, -2 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 95.1, 28, -2 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 95.1, -28, -2 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-109.1,33.1,-2.2),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-109.1,-33.1,-2.2),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-109.1,25.2,-2.2),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-109.1,-25.2,-2.2),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-109.1,24,-2.2),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-109.1,-24,-2.2),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-109.1,40,-2.2),
+			Vector( 93, 39, -2 ),
+		},
+		Right = {
+			Vector(-109.1,-40,-2.2),
+			Vector( 93, -39, -2 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "elegant", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 95.1, 28, 4.6 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 95.1, -28, 4.6 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-107.6,33.8,7.5),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-107.6,-33.8,7.5),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 102.2, 21, 4.6 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 102.2, -21, 4.6 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 102.2, 24, 4.6 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 102.2, -24, 4.6 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 102.2, 30, 4.6 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+		{pos = Vector( 102.2, -30, 4.6 ),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,100)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 95.1, 28, -2 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 95.1, -28, -2 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-107.6,33.8,7.5),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-107.6,-33.8,7.5),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-107.6,11,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,-11,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,14,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,-14,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,17,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,-17,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,20,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,-20,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,23,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,-23,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,26,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-107.6,-26,7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-107.6,29,7.5),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-107.6,-29,7.5),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-107.6,29,7.5),
+			Vector( 100, 35, 4.6 ),
+		},
+		Right = {
+			Vector(-107.6,-29,7.5),
+			Vector( 100, -35, 4.6 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "emperor", light_table)
--- lua/autorun/simfphys_gta_sa_sedans_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_sedans_spawnlist.lua
@@ -49,10 +49,7 @@ local V = {
 		
 		ExhaustPositions = {
 			{
-				pos = Vector(-87,-17,-15),
-				ang = Angle(90,180,0),
-			},{
-				pos = Vector(-87,17,-15),
+				pos = Vector(-90,-17,-15),
 				ang = Angle(90,180,0),
 			}
 		},
@@ -78,10 +75,10 @@ local V = {
 		BrakePower = 34,
 		
 		IdleRPM = 800,
-		LimitRPM = 7800,
+		LimitRPM = 7400,
 		PeakTorque = 150,
 		PowerbandStart = 900,
-		PowerbandEnd = 7600,
+		PowerbandEnd = 6900,
 		Turbocharged = false,
 		Supercharged = false,
 		
@@ -94,30 +91,272 @@ local V = {
 		EngineSoundPreset = 0,
 
 		
-		Sound_Idle = "bank_001/sound_002.wav",
+		Sound_Idle = "bank_080/sound_002.wav",
 		Sound_IdlePitch = 1,
 		
-		Sound_Mid = "bank_001/sound_001.wav",
+		Sound_Mid = "bank_080/sound_001.wav",
 		Sound_MidPitch = 1,
 		Sound_MidVolume = 1,
 		Sound_MidFadeOutRPMpercent = 80,
 		Sound_MidFadeOutRate = 0.8,
 		
-		Sound_High = "bank_001/sound_001.wav",
+		Sound_High = "bank_080/sound_001.wav",
 		Sound_HighPitch = 1.1,
 		Sound_HighVolume = 1.5,
 		Sound_HighFadeInRPMpercent = 80,
 		Sound_HighFadeInRate = 0.8,
 		
-		Sound_Throttle = "bank_002/sound_002.wav",
+		Sound_Throttle = "bank_081/sound_002.wav",
 		Sound_ThrottlePitch = 1,
 		Sound_ThrottleVolume = 4,
 		
 		--
 		snd_horn = "bank_068/sound_002.wav",
 		
-		DifferentialGear = 0.6,
+		DifferentialGear = 0.45,
 		Gears = {-0.12,0,0.1,0.17,0.25,0.34,0.45}
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_admiral", V )
+
+local V = {
+	Name = "Elegant",
+	Model = "models/GTA_SA/sedans/elegant.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Sedans",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 2200,
+		
+		EnginePos = Vector(60,0,10),
+		
+		LightsTable = "elegant",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/GTA_SA/sedans/elegant_wheel.mdl",
+		CustomWheelPosFL = Vector(64.8,37.1,-16.2),
+		CustomWheelPosFR = Vector(64.8,-37.1,-16.2),
+		CustomWheelPosRL = Vector(-65.2,37.1,-16.2),
+		CustomWheelPosRR = Vector(-65.2,-37.1,-16.2),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(10,0,0),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(-5,-18,15),
+		SeatPitch = -10,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(4,-17,-15),
+				ang = Angle(0,-90,20)
+			},
+			{
+				pos = Vector(-35,17,-16),
+				ang = Angle(0,-90,18)
+			},
+			{
+				pos = Vector(-35,-17,-16),
+				ang = Angle(0,-90,18)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-106.6,-17.6,-23),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 11,
+		FrontConstant = 50000,
+		FrontDamping = 1300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 10,
+		RearConstant = 50000,
+		RearDamping = 1300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 70,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 34,
+		
+		IdleRPM = 800,
+		LimitRPM = 7400,
+		PeakTorque = 180,
+		PowerbandStart = 900,
+		PowerbandEnd = 6900,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-82.8,41.7,4),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 60,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_080/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_080/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_080/sound_001.wav",
+		Sound_HighPitch = 1.1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_081/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.45,
+		Gears = {-0.12,0,0.1,0.17,0.25,0.34,0.45}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_elegant", V )
+
+local V = {
+	Name = "Emperor",
+	Model = "models/GTA_SA/sedans/emperor.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Sedans",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 1800,
+		
+		EnginePos = Vector(60,0,10),
+		
+		LightsTable = "emperor",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/GTA_SA/sedans/emperor_wheel.mdl",
+		CustomWheelPosFL = Vector(64.8,36,-8),
+		CustomWheelPosFR = Vector(64.8,-36,-8),
+		CustomWheelPosRL = Vector(-64.4,36,-8),
+		CustomWheelPosRR = Vector(-64.4,-36,-8),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(10,0,0),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(-5,-18,18),
+		SeatPitch = -10,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(4,-17,-12),
+				ang = Angle(0,-90,20)
+			},
+			{
+				pos = Vector(-35,17,-12),
+				ang = Angle(0,-90,18)
+			},
+			{
+				pos = Vector(-35,-17,-12),
+				ang = Angle(0,-90,18)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-113,17,-10),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 9,
+		FrontConstant = 42000,
+		FrontDamping = 1300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 8,
+		RearConstant = 42000,
+		RearDamping = 1300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 70,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 34,
+		
+		IdleRPM = 800,
+		LimitRPM = 7400,
+		PeakTorque = 170,
+		PowerbandStart = 900,
+		PowerbandEnd = 6900,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-82.8,-40.5,7.2),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 60,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_080/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_080/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_080/sound_001.wav",
+		Sound_HighPitch = 1.1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_081/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.45,
+		Gears = {-0.12,0,0.1,0.17,0.25,0.34,0.45}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_emperor", V )
