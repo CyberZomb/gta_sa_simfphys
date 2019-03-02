6411d5d75a620b2819867a80150e8a0e75fc74dd
def-valerius <31399259+def-valerius@users.noreply.github.com>
2019-03-02 12:44:23 +0500

    Entity
    Novelty: BF Injection

--- lua/autorun/simfphys_gta_sa_novelty_lights.lua
+++ lua/autorun/simfphys_gta_sa_novelty_lights.lua
@@ -46,3 +46,52 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "bandito", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 42.5, 25.5, 7.9 ),
+	L_HeadLampAng = Angle(14,0,0),
+	R_HeadLampPos = Vector( 42.5, -25.5, 7.9 ),
+	R_HeadLampAng = Angle(14,0,0),
+	
+	L_RearLampPos = Vector(-47.6,20.5,8.6),
+	L_RearLampAng = Angle(10,180,0),
+	R_RearLampPos = Vector(-47.6,-20.5,8.6),
+	R_RearLampAng = Angle(10,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 52.5, 25.5, 7.9 ),material = "sprites/light_ignorez",size = 25, color = Color( 255,230,230,120)},
+		{pos = Vector( 52.5, -25.5, 7.9 ),material = "sprites/light_ignorez",size = 25, color = Color( 255,230,230,120)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 52.5, 25.5, 7.9 ),material = "sprites/light_ignorez",size = 30, color = Color( 255,235,220,220)},
+		{pos = Vector( 52.5, -25.5, 7.9 ),material = "sprites/light_ignorez",size = 30, color = Color( 255,235,220,220)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-57.6,20.5,8.6),material = "sprites/light_ignorez",size = 20,color = Color( 255, 0, 0,  120)},
+		{pos = Vector(-57.6,-20.5,8.6),material = "sprites/light_ignorez",size = 20,color = Color( 255, 0, 0,  120)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-57.6,20.5,8.6),material = "sprites/light_ignorez",size = 28,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-57.6,-20.5,8.6),material = "sprites/light_ignorez",size = 28,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-57.6,17.5,8.6),material = "sprites/light_ignorez",size = 20,color = Color( 255, 255, 255, 200)},
+		{pos = Vector(-57.6,-17.5,8.6),material = "sprites/light_ignorez",size = 20,color = Color( 255, 255, 255, 200)},
+	},
+	
+	DelayOn = 0,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-57.6,17.5,8.6),
+			Vector( 52.5, 25.9, 7.9 ),
+		},
+		Right = {
+			Vector(-57.6,-17.5,8.6),
+			Vector( 52.5, -25.9, 7.9 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "bfinject", light_table)
--- lua/autorun/simfphys_gta_sa_novelty_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_novelty_spawnlist.lua
@@ -121,3 +121,127 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_bandito", V )
+
+local V = {
+	Name = "BF Injection",
+	Model = "models/GTA_SA/novelty/bfinject.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Novelty",
+	SpawnOffset = Vector(0,0,10),
+	SpawnAngleOffset = 90,
+	
+	Members = {
+		Mass = 1200,
+		
+		EnginePos = Vector(-40,0,8),
+		
+		LightsTable = "bfinject",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 1,
+		
+		CustomWheelModel = "models/GTA_SA/novelty/bfinject_wheel.mdl",
+		CustomWheelPosFL = Vector(38.8,35.6,-9.7),
+		CustomWheelPosFR = Vector(38.8,-35.6,-9.7),
+		CustomWheelPosRL = Vector(-38.8,36,-10),
+		CustomWheelPosRR = Vector(-38.8,-36,-10),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(4,0,0),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(-11,-14,19),
+		SeatPitch = -7,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(0,-14,-10),
+				ang = Angle(0,-90,26)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-59,-18.3,-8.2),
+				ang = Angle(70,180,0),
+			},
+			{
+				pos = Vector(-59,18.3,-8.2),
+				ang = Angle(70,180,0),
+			},
+			{
+				pos = Vector(-59,-18.3,-8.2),
+				ang = Angle(70,180,0),
+			},
+			{
+				pos = Vector(-59,18.3,-8.2),
+				ang = Angle(70,180,0),
+			}
+		},
+		
+		FrontHeight = 7,
+		FrontConstant = 30000,
+		FrontDamping = 1300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 6,
+		RearConstant = 30000,
+		RearDamping = 1300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 4,
+		
+		MaxGrip = 55,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 40,
+		
+		IdleRPM = 300,
+		LimitRPM = 5000,
+		PeakTorque = 200,
+		PowerbandStart = 600,
+		PowerbandEnd = 4500,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-25.5,11.5,13.3),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 30,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_069/sound_002.wav",
+		Sound_IdlePitch = 1.3,
+		
+		Sound_Mid = "bank_069/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_069/sound_001.wav",
+		Sound_HighPitch = 1.2,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_069/sound_001.wav",
+		Sound_ThrottlePitch = 1.1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.17,0,0.15,0.28,0.44,0.61}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_bfinject", V )
