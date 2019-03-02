ffce140031a72ba5dfbbf29ed762e56bd1dfd6cf
def-valerius <31399259+def-valerius@users.noreply.github.com>
2019-03-02 18:50:47 +0500

    Entities
    Novelty: Caddy, Camper

--- lua/autorun/simfphys_gta_sa_novelty_lights.lua
+++ lua/autorun/simfphys_gta_sa_novelty_lights.lua
@@ -95,3 +95,101 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "bfinject", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 32.8, 15.8, 2.6 ),
+	L_HeadLampAng = Angle(14,0,0),
+	R_HeadLampPos = Vector( 32.8, -15.8, 2.6 ),
+	R_HeadLampAng = Angle(14,0,0),
+	
+	L_RearLampPos = Vector(-37.1,18.3,2.8),
+	L_RearLampAng = Angle(10,180,0),
+	R_RearLampPos = Vector(-37.1,-18.3,2.8),
+	R_RearLampAng = Angle(10,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 42.8, 15.8, 2.6 ),material = "sprites/light_ignorez",size = 25, color = Color( 255,230,230,120)},
+		{pos = Vector( 42.8, -15.8, 2.6 ),material = "sprites/light_ignorez",size = 25, color = Color( 255,230,230,120)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 42.8, 15.8, 2.6 ),material = "sprites/light_ignorez",size = 30, color = Color( 255,235,220,220)},
+		{pos = Vector( 42.8, -15.8, 2.6 ),material = "sprites/light_ignorez",size = 30, color = Color( 255,235,220,220)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-47.1,18.3,2.8),material = "sprites/light_ignorez",size = 20,color = Color( 255, 0, 0,  120)},
+		{pos = Vector(-47.1,-18.3,2.8),material = "sprites/light_ignorez",size = 20,color = Color( 255, 0, 0,  120)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-47.1,18.3,2.8),material = "sprites/light_ignorez",size = 28,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-47.1,-18.3,2.8),material = "sprites/light_ignorez",size = 28,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-47.1,18.3,2.8),material = "sprites/light_ignorez",size = 20,color = Color( 255, 255, 255, 200)},
+		{pos = Vector(-47.1,-18.3,2.8),material = "sprites/light_ignorez",size = 20,color = Color( 255, 255, 255, 200)},
+	},
+	
+	DelayOn = 0,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-47.1,18.3,2.8),
+			Vector( 42.8, 17, 2.6 ),
+		},
+		Right = {
+			Vector(-47.1,-18.3,2.8),
+			Vector( 42.8, -17, 2.6 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "caddy", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 90, 24.8, -12.6 ),
+	L_HeadLampAng = Angle(14,0,0),
+	R_HeadLampPos = Vector( 90, -24.8, -12.6 ),
+	R_HeadLampAng = Angle(14,0,0),
+	
+	L_RearLampPos = Vector(-89,24,-9),
+	L_RearLampAng = Angle(10,180,0),
+	R_RearLampPos = Vector(-89,-24,-9),
+	R_RearLampAng = Angle(10,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 100, 24.8, -12.6 ),material = "sprites/light_ignorez",size = 30, color = Color( 255,230,230,120)},
+		{pos = Vector( 100, -24.8, -12.6 ),material = "sprites/light_ignorez",size = 30, color = Color( 255,230,230,120)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 100, 24.8, -12.6 ),material = "sprites/light_ignorez",size = 32, color = Color( 255,235,220,220)},
+		{pos = Vector( 100, -24.8, -12.6 ),material = "sprites/light_ignorez",size = 32, color = Color( 255,235,220,220)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-99,24,-9),material = "sprites/light_ignorez",size = 20,color = Color( 255, 0, 0,  120)},
+		{pos = Vector(-99,-24,-9),material = "sprites/light_ignorez",size = 20,color = Color( 255, 0, 0,  120)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-99,24,-9),material = "sprites/light_ignorez",size = 28,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-99,-24,-9),material = "sprites/light_ignorez",size = 28,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-99,24,-11),material = "sprites/light_ignorez",size = 20,color = Color( 255, 255, 255, 200)},
+		{pos = Vector(-99,-24,-11),material = "sprites/light_ignorez",size = 20,color = Color( 255, 255, 255, 200)},
+	},
+	
+	DelayOn = 0,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-99,24,-11),
+			Vector( 93.9, 28.8, -0.7 ),
+		},
+		Right = {
+			Vector(-99,-24,-11),
+			Vector( 93.9, -28.8, -0.7 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "camper", light_table)
--- lua/autorun/simfphys_gta_sa_novelty_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_novelty_spawnlist.lua
@@ -209,7 +209,7 @@ local V = {
 		Turbocharged = false,
 		Supercharged = false,
 		
-		FuelFillPos = Vector(-25.5,11.5,13.3),
+		FuelFillPos = Vector(38.5,-6.4,12.2),
 		FuelType = FUELTYPE_PETROL,
 		FuelTankSize = 30,
 		
@@ -245,3 +245,235 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_bfinject", V )
+
+local V = {
+	Name = "Caddy",
+	Model = "models/GTA_SA/novelty/caddy.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Novelty",
+	SpawnOffset = Vector(0,0,10),
+	SpawnAngleOffset = 90,
+	
+	Members = {
+		Mass = 1000,
+		
+		EnginePos = Vector(30,0,8),
+		
+		LightsTable = "caddy",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 1,
+		
+		CustomWheelModel = "models/GTA_SA/novelty/caddy_wheel.mdl",
+		CustomWheelPosFL = Vector(34.9,19.8,-9.7),
+		CustomWheelPosFR = Vector(34.9,-19.8,-9.7),
+		CustomWheelPosRL = Vector(-34.9,19.8,-10),
+		CustomWheelPosRR = Vector(-34.9,-19.8,-10),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(4,0,0),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(-19,-12,30),
+		SeatPitch = 6,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(-10,-12,-1),
+				ang = Angle(0,-90,9)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-48.9,-16.9,-10),
+				ang = Angle(70,180,0),
+			}
+		},
+		
+		FrontHeight = 7,
+		FrontConstant = 40000,
+		FrontDamping = 1300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 6,
+		RearConstant = 40000,
+		RearDamping = 1300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 3,
+		
+		MaxGrip = 50,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 30,
+		
+		IdleRPM = 300,
+		LimitRPM = 5000,
+		PeakTorque = 90,
+		PowerbandStart = 600,
+		PowerbandEnd = 4999,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-25.5,11.5,13.3),
+		FuelType = FUELTYPE_ELECTRIC,
+		FuelTankSize = 50,
+		
+		PowerBias = 0.2,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "",
+		Sound_IdlePitch = 0.5,
+		
+		Sound_Mid = "bank_057/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_057/sound_001.wav",
+		Sound_HighPitch = 1.2,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_057/sound_001.wav",
+		Sound_ThrottlePitch = 1.1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.2,0,0.2,0.36,0.6}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_caddy", V )
+
+local V = {
+	Name = "Camper",
+	Model = "models/GTA_SA/novelty/camper.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Novelty",
+	SpawnOffset = Vector(0,0,30),
+	SpawnAngleOffset = 90,
+	
+	Members = {
+		Mass = 1900,
+		
+		EnginePos = Vector(-60,0,8),
+		
+		LightsTable = "camper",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 1,
+		
+		CustomWheelModel = "models/GTA_SA/novelty/camper_wheel.mdl",
+		CustomWheelPosFL = Vector(60.8,28.4,-23.7),
+		CustomWheelPosFR = Vector(60.8,-28.4,-23.7),
+		CustomWheelPosRL = Vector(-74.1,27.7,-23.7),
+		CustomWheelPosRR = Vector(-74.1,-27.7,-23.7),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(-1,0,5),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(54,-12,23),
+		SeatPitch = 6,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(60,-12,-17),
+				ang = Angle(0,-90,9)
+			},
+			{
+				pos = Vector(2,-16,-19),
+				ang = Angle(0,-90,9)
+			},
+			{
+				pos = Vector(2,7,-19),
+				ang = Angle(0,-90,9)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-101,11.8,-32),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 8,
+		FrontConstant = 45000,
+		FrontDamping = 2300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 7,
+		RearConstant = 45000,
+		RearDamping = 2300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 3,
+		
+		MaxGrip = 50,
+		Efficiency = 1,
+		GripOffset = -2,
+		BrakePower = 30,
+		
+		IdleRPM = 300,
+		LimitRPM = 5000,
+		PeakTorque = 160,
+		PowerbandStart = 600,
+		PowerbandEnd = 4500,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-90.3,-33.4,-1),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 40,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_135/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_135/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_135/sound_001.wav",
+		Sound_HighPitch = 1.2,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_135/sound_002.wav",
+		Sound_ThrottlePitch = 1.1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.15,0,0.15,0.25,0.32,0.41,0.52}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_camper", V )
