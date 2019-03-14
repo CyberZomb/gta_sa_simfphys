15bba2d578e080a8cd7183e2d570c052ca3d8d57
def-valerius <31399259+def-valerius@users.noreply.github.com>
2019-03-14 21:58:39 +0500

    Entities
    Government: Barracks, Enforcer, FBI Truck, FBI Ranch

--- lua/autorun/simfphys_gta_sa_government_lights.lua
+++ lua/autorun/simfphys_gta_sa_government_lights.lua
@@ -191,3 +191,268 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "barracks", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 105.9, 31.6, -4.6 ),
+	L_HeadLampAng = Angle(10,0,0),
+	R_HeadLampPos = Vector( 105.9, -31.6, -4.6 ),
+	R_HeadLampAng = Angle(10,0,0),
+	
+	L_RearLampPos = Vector(-131.4,34.9,-13.3),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-131.4,-34.9,-13.3),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 115.9, 31.6, -4.6 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,230,230,180)},
+		{pos = Vector( 115.9, -31.6, -4.6 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,230,230,180)},
+		{pos = Vector( 54.3, 12.6, 47.8),material = "sprites/light_ignorez",size = 17, color = Color( 255,125,45,120)},
+		{pos = Vector( 54.3, -12.6, 47.8),material = "sprites/light_ignorez",size = 17, color = Color( 255,125,45,120)},
+		{pos = Vector( 54.3, 0, 47.8),material = "sprites/light_ignorez",size = 17, color = Color( 255,125,45,120)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 115.9, 31.6, -4.6 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 115.9, -31.6, -4.6 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-141.4,34.9,-13.3),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  190)},
+		{pos = Vector(-141.4,-34.9,-13.3),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  190)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-140.4,34.9,22.6),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-140.4,-34.9,22.6),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-140.4,34.9,11.5),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-140.4,-34.9,11.5),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-140.4,34.9,11.5),
+			Vector( 114.1, 43.2, -20.8 ),
+			Vector( 97.2, 34.2, 9.3 ),
+		},
+		Right = {
+			Vector(-140.4,-34.9,11.5),
+			Vector( 114.1, -43.2, -20.8 ),
+			Vector( 97.2, 34.2, 9.3 ),
+		},
+	},
+	ems_sounds = {"bank_068/sound_011.wav"},
+	ems_sprites = {
+		{   --1
+			pos = Vector(39.9,-19,55),
+			material = "sprites/light_glow02_add_noz",
+			size = 38,
+			Colors = {Color(0,0,0,0),Color(255,0,0,255)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{
+			pos = Vector(39.9,19,55),
+			material = "sprites/light_glow02_add_noz",
+			size = 38,
+			Colors = {Color(255,0,0,255),Color(0,0,0,0)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{   --2
+			pos = Vector(3.6,-43.2,45.3),
+			material = "sprites/light_glow02_add_noz",
+			size = 38,
+			Colors = {Color(0,0,0,0),Color(255,145,0,255)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{
+			pos = Vector(3.6,43.2,45.3),
+			material = "sprites/light_glow02_add_noz",
+			size = 38,
+			Colors = {Color(255,145,0,255),Color(0,0,0,0)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{   --3
+			pos = Vector(3.6,-43.2,33.8),
+			material = "sprites/light_glow02_add_noz",
+			size = 32,
+			Colors = {Color(0,0,0,0),Color(255,0,0,255)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{
+			pos = Vector(3.6,43.2,33.8),
+			material = "sprites/light_glow02_add_noz",
+			size = 32,
+			Colors = {Color(255,0,0,255),Color(0,0,0,0)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{   --4
+			pos = Vector(-58.6,-43.2,45.3),
+			material = "sprites/light_glow02_add_noz",
+			size = 38,
+			Colors = {Color(0,0,0,0),Color(255,145,0,255)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{
+			pos = Vector(-58.6,43.2,45.3),
+			material = "sprites/light_glow02_add_noz",
+			size = 38,
+			Colors = {Color(255,145,0,255),Color(0,0,0,0)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{   --5
+			pos = Vector(-58.6,-43.2,33.8),
+			material = "sprites/light_glow02_add_noz",
+			size = 32,
+			Colors = {Color(0,0,0,0),Color(255,0,0,255)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{
+			pos = Vector(-58.6,43.2,33.8),
+			material = "sprites/light_glow02_add_noz",
+			size = 32,
+			Colors = {Color(255,0,0,255),Color(0,0,0,0)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{   --6
+			pos = Vector(-120.9,-43.2,45.3),
+			material = "sprites/light_glow02_add_noz",
+			size = 38,
+			Colors = {Color(0,0,0,0),Color(255,145,0,255)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{
+			pos = Vector(-120.9,43.2,45.3),
+			material = "sprites/light_glow02_add_noz",
+			size = 38,
+			Colors = {Color(255,145,0,255),Color(0,0,0,0)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{   --7
+			pos = Vector(-120.9,-43.2,33.8),
+			material = "sprites/light_glow02_add_noz",
+			size = 32,
+			Colors = {Color(0,0,0,0),Color(255,0,0,255)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+		{
+			pos = Vector(-120.9,43.2,33.8),
+			material = "sprites/light_glow02_add_noz",
+			size = 32,
+			Colors = {Color(255,0,0,255),Color(0,0,0,0)}, -- the script will go from color to color
+			Speed = 0.4, -- for how long each color will be drawn
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "enforcer", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 81, 30.6, 4.3 ),
+	L_HeadLampAng = Angle(10,0,0),
+	R_HeadLampPos = Vector( 81, -30.6, 4.3 ),
+	R_HeadLampAng = Angle(10,0,0),
+	
+	L_RearLampPos = Vector(-85.4,28.8,-11.1),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-85.4,-28.8,-11.1),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 91, 30.6, 4.3 ),material = "sprites/light_ignorez",size = 42, color = Color( 255,230,230,180)},
+		{pos = Vector( 91, -30.6, 4.3 ),material = "sprites/light_ignorez",size = 42, color = Color( 255,230,230,180)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 91, 30.6, 4.3 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+		{pos = Vector( 91, -30.6, 4.3 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-95.4,28.8,-11.1),material = "sprites/light_ignorez",size = 30,color = Color( 255, 0, 0,  190)},
+		{pos = Vector(-95.4,-28.8,-11.1),material = "sprites/light_ignorez",size = 30,color = Color( 255, 0, 0,  190)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-95.4,28.8,-11.1),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-95.4,-28.8,-11.1),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-95.4,28.8,-11.1),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-95.4,-28.8,-11.1),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-95.4,28.8,-11.1),
+			Vector( 92.8, 30.6, -5.4 ),
+		},
+		Right = {
+			Vector(-95.4,-28.8,-11.1),
+			Vector( 92.8, -30.6, -5.4 ),
+		},
+	},
+	ems_sounds = {"bank_068/sound_011.wav"}
+
+}
+list.Set( "simfphys_lights", "fbitruck", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 93, 30, 1 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 93, -30, 1 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-96,41,-3),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-96,-41,-3),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 113, 30, 1 ),material = "sprites/light_ignorez",size = 42, color = Color( 255,230,210,150)},
+		{pos = Vector( 113, -30, 1 ),material = "sprites/light_ignorez",size = 42, color = Color( 255,230,210,150)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 113, 30, 1 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+		{pos = Vector( 113, -30, 1 ),material = "sprites/light_ignorez",size = 45, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-111.9,41,-3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-111.9,-41,-3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-111.9,41,-3),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-111.9,-41,-3),material = "sprites/light_ignorez",size = 40,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-111.9,41,-3),material = "sprites/light_ignorez",size = 25,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-111.9,-41,-3),material = "sprites/light_ignorez",size = 25,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			{pos = Vector(-111.9,41,5),material = "sprites/light_ignorez",size = 25,color = Color( 255, 205, 50, 250)},
+			Vector( 119, 31, -16 ),
+			{pos = Vector( 112, 40, -4 ),material = "sprites/light_ignorez",size = 15,color = Color( 255, 205, 50, 250)},
+			{pos = Vector( 112, 40, -1 ),material = "sprites/light_ignorez",size = 15,color = Color( 255, 205, 50, 250)},
+			{pos = Vector( 112, 40, 2 ),material = "sprites/light_ignorez",size = 15,color = Color( 255, 205, 50, 250)},
+			{pos = Vector( 112, 40, 5 ),material = "sprites/light_ignorez",size = 15,color = Color( 255, 205, 50, 250)},
+			
+		},
+		Right = {
+			{pos = Vector(-111.9,-41,5),material = "sprites/light_ignorez",size = 25,color = Color( 255, 205, 50, 250)},
+			Vector( 119, -31, -16 ),
+			{pos = Vector( 112.5, -40, -4 ),material = "sprites/light_ignorez",size = 15,color = Color( 255, 205, 50, 250)},
+			{pos = Vector( 112.5, -40, -1 ),material = "sprites/light_ignorez",size = 15,color = Color( 255, 205, 50, 250)},
+			{pos = Vector( 112.5, -40, 2 ),material = "sprites/light_ignorez",size = 15,color = Color( 255, 205, 50, 250)},
+			{pos = Vector( 112.5, -40, 5 ),material = "sprites/light_ignorez",size = 15,color = Color( 255, 205, 50, 250)},
+			
+		},
+	},
+	ems_sounds = {"bank_068/sound_011.wav"}
+
+}
+list.Set( "simfphys_lights", "fbiranch", light_table)
--- lua/autorun/simfphys_gta_sa_government_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_government_spawnlist.lua
@@ -243,3 +243,375 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_barracks", V )
+
+local V = {
+	Name = "Enforcer",
+	Model = "models/GTA_SA/government/enforcer.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Government",
+	SpawnOffset = Vector(0,0,50),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 4000,
+		
+		EnginePos = Vector(100,0,0),
+		
+		LightsTable = "enforcer",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 0,
+		
+		CustomWheelModel = "models/GTA_SA/government/enforcer_wheel.mdl",
+		CustomWheelPosFL = Vector(90.7,32.7,-28),
+		CustomWheelPosFR = Vector(90.7,-32.7,-28),
+		CustomWheelPosRL = Vector(-82,36.3,-28),
+		CustomWheelPosRR = Vector(-82,-36.3,-28),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(3,0,15),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(30,-14,40),
+		SeatPitch = 12,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(41,-15,0),
+				ang = Angle(0,-90,8)
+			},
+			{
+				pos = Vector(-40,-27,0),
+				ang = Angle(0,0,8)
+			},
+			{
+				pos = Vector(-40,27,0),
+				ang = Angle(0,180,8)
+			},
+			{
+				pos = Vector(-70,-27,0),
+				ang = Angle(0,0,8)
+			},
+			{
+				pos = Vector(-70,27,0),
+				ang = Angle(0,180,8)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-139.3,-21.2,-25.9),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		StrengthenSuspension = true,
+		
+		FrontHeight = 0,
+		FrontConstant = 50000,
+		FrontDamping = 3000,
+		FrontRelativeDamping = 2000,
+		
+		RearHeight = 0,
+		RearConstant = 50000,
+		RearDamping = 3000,
+		RearRelativeDamping = 2000,
+		
+		FastSteeringAngle = 35,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 2,
+		
+		MaxGrip = 150,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 60,
+		
+		IdleRPM = 500,
+		LimitRPM = 5000,
+		PeakTorque = 320,
+		PowerbandStart = 650,
+		PowerbandEnd = 4700,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-114.8,42.8,-5.7),
+		FuelType = FUELTYPE_DIESEL,
+		FuelTankSize = 90,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_074/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_074/sound_001.wav",
+		Sound_MidPitch = 1.5,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_074/sound_001.wav",
+		Sound_HighPitch = 1.1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_075/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_012.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.1,0,0.1,0.18,0.27,0.39,0.52}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_enforcer", V )
+
+local V = {
+	Name = "FBI Truck",
+	Model = "models/GTA_SA/government/fbitruck.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Government",
+	SpawnOffset = Vector(0,0,50),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 4000,
+		
+		EnginePos = Vector(60,0,0),
+		
+		LightsTable = "fbitruck",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 0,
+		
+		CustomWheelModel = "models/GTA_SA/government/fbitruck_wheel.mdl",
+		CustomWheelPosFL = Vector(62.2,33.8,-27),
+		CustomWheelPosFR = Vector(62.2,-33.8,-27),
+		CustomWheelPosRL = Vector(-55,33.8,-27),
+		CustomWheelPosRR = Vector(-55,-33.8,-27),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(3,0,5),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(-13,-17,20),
+		SeatPitch = 12,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(1,-17,-12),
+				ang = Angle(0,-90,8)
+			},
+			{
+				pos = Vector(-64,0,-12),
+				ang = Angle(0,90,8)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-91,22.3,-24.4),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		StrengthenSuspension = true,
+		
+		FrontHeight = 0,
+		FrontConstant = 50000,
+		FrontDamping = 3000,
+		FrontRelativeDamping = 2000,
+		
+		RearHeight = 0,
+		RearConstant = 50000,
+		RearDamping = 3000,
+		RearRelativeDamping = 2000,
+		
+		FastSteeringAngle = 35,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 2,
+		
+		MaxGrip = 150,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 60,
+		
+		IdleRPM = 500,
+		LimitRPM = 5000,
+		PeakTorque = 320,
+		PowerbandStart = 650,
+		PowerbandEnd = 4700,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-72.7,38.8,3.9),
+		FuelType = FUELTYPE_DIESEL,
+		FuelTankSize = 90,
+		
+		PowerBias = 0.7,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_074/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_074/sound_001.wav",
+		Sound_MidPitch = 1.5,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_074/sound_001.wav",
+		Sound_HighPitch = 1.1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_075/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_012.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.1,0,0.1,0.18,0.27,0.39,0.52}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_fbitruck", V )
+
+local V = {
+	Name = "FBI Rancher",
+	Model = "models/GTA_SA/government/fbiranch.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Government",
+	SpawnOffset = Vector(0,0,30),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 3500,
+		
+		EnginePos = Vector(80,0,10),
+		
+		LightsTable = "fbiranch",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/GTA_SA/government/fbiranch_wheel.mdl",
+		CustomWheelPosFL = Vector(82.4,37,-33),
+		CustomWheelPosFR = Vector(82.4,-37,-33),
+		CustomWheelPosRL = Vector(-69.4,37,-33),
+		CustomWheelPosRR = Vector(-69.4,-37,-33),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(6,0,18),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(16,-18,23),
+		SeatPitch = -2,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(22,-18,-10),
+				ang = Angle(0,-90,15)
+			},
+			{
+				pos = Vector(-18,18,-10),
+				ang = Angle(0,-90,15)
+			},
+			{
+				pos = Vector(-18,-18,-10),
+				ang = Angle(0,-90,15)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-115.5,-13.6,-22),
+				ang = Angle(90,180,0),
+			},
+			{
+				pos = Vector(-115.5,-17,-22),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 1,
+		FrontConstant = 50000,
+		FrontDamping = 6300,
+		FrontRelativeDamping = 600,
+		
+		RearHeight = 0,
+		RearConstant = 50000,
+		RearDamping = 6300,
+		RearRelativeDamping = 600,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 85,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 30,
+		
+		IdleRPM = 700,
+		LimitRPM = 4800,
+		PeakTorque = 240,
+		PowerbandStart = 800,
+		PowerbandEnd = 4600,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-44.6,-43.8,0),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 58,
+		
+		PowerBias = 0.1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_092/sound_002.wav",
+		Sound_IdlePitch = 0.8,
+		
+		Sound_Mid = "bank_092/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_092/sound_001.wav",
+		Sound_HighPitch = 1.3,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_093/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.12,0,0.08,0.17,0.28,0.39,0.5}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_fbiranch", V )
