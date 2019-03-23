6a86dbe51d82f8154f7807c97136477027f5261a
def-valerius <31399259+def-valerius@users.noreply.github.com>
2019-03-23 13:25:22 +0500

    Entities
    Coupes And Hatchbacks: Buccaneer, Cadrona

--- lua/autorun/simfphys_gta_sa_coupes_hatchbacks_ligths.lua
+++ lua/autorun/simfphys_gta_sa_coupes_hatchbacks_ligths.lua
@@ -111,3 +111,107 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "bravura", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 86.1, 29.8, -1.4 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 86.1, -29.8, -1.4 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-91.5,36.7,-0.3),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-91.5,-36.7,-0.3),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 96.1, 29.8, -1.4 ),material = "sprites/light_ignorez",size = 40, color = Color( 255,230,230,100)},
+		{pos = Vector( 96.1, -29.8, -1.4 ),material = "sprites/light_ignorez",size = 40, color = Color( 255,230,230,100)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 96.1, 29.8, -1.4 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 96.1, -29.8, -1.4 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-101.5,36.7,-0.3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-101.5,-36.7,-0.3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-101.5,36.7,-7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-101.5,-36.7,-7.5),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-101.5,36.7,-5),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-101.5,-36.7,-5),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-101.5,36.7,-5),
+			Vector( 96.1, 32.4, -4.3 ),
+		},
+		Right = {
+			Vector(-101.5,-36.7,-5),
+			Vector( 96.1, -32.4, -4.3 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "buccanee", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 82.8, 25.2, 0.7 ),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 82.8, -25.2, 0.7 ),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-76,25.2,5),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-76,-25.2,5),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 92.8, 25.2, 0.7 ),material = "sprites/light_ignorez",size = 47, color = Color( 255,230,230,100)},
+		{pos = Vector( 92.8, -25.2, 0.7 ),material = "sprites/light_ignorez",size = 47, color = Color( 255,230,230,100)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 92.8, 25.2, 0.7 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 92.8, -25.2, 0.7 ),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-85,32,4),material = "sprites/light_ignorez",size = 38,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-85,-32,4),material = "sprites/light_ignorez",size = 38,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-86,25.2,3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-86,-25.2,3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-86,21,3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-86,-21,3),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-86,16,3),material = "sprites/light_ignorez",size = 33,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-86,-16,3),material = "sprites/light_ignorez",size = 33,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-86,11,3),material = "sprites/light_ignorez",size = 33,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-86,-11,3),material = "sprites/light_ignorez",size = 33,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-83.8,31.6,0.6),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-83.8,-31.6,0.6),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-85,32,4),
+			Vector( 87.8, 32.7, 0.6 ),
+		},
+		Right = {
+			Vector(-85,-32,4),
+			Vector( 87.8, -32.7, 0.6 ),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "cadrona", light_table)
--- lua/autorun/simfphys_gta_sa_coupes_hatchbacks_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_coupes_hatchbacks_spawnlist.lua
@@ -223,3 +223,227 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_bravura", V )
+
+local V = {
+	Name = "Buccaneer",
+	Model = "models/GTA_SA/coupes_hatchbacks/buccanee.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Coupes & Hatchbacks",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 1700,
+		
+		EnginePos = Vector(60,0,10),
+		
+		LightsTable = "buccanee",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/GTA_SA/coupes_hatchbacks/buccanee_wheel.mdl",
+		CustomWheelPosFL = Vector(64.8,33.8,-13),
+		CustomWheelPosFR = Vector(64.8,-33.8,-13),
+		CustomWheelPosRL = Vector(-52.5,33.8,-13),
+		CustomWheelPosRR = Vector(-52.5,-33.8,-13),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(10,0,4),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(-4,-19,13),
+		SeatPitch = -5,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(6,-18,-16),
+				ang = Angle(0,-90,25)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-102.5,15.1,-16.2),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 8,
+		FrontConstant = 38000,
+		FrontDamping = 1300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 7,
+		RearConstant = 38000,
+		RearDamping = 1300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 58,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 30,
+		
+		IdleRPM = 700,
+		LimitRPM = 4800,
+		PeakTorque = 180,
+		PowerbandStart = 800,
+		PowerbandEnd = 4600,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-78.4,-42,-2.1),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 55,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_039/sound_002.wav",
+		Sound_IdlePitch = 0.8,
+		
+		Sound_Mid = "bank_039/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_039/sound_001.wav",
+		Sound_HighPitch = 1.3,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_039/sound_001.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.12,0,0.12,0.22,0.33,0.48,0.61}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_buccanee", V )
+
+local V = {
+	Name = "Cadrona",
+	Model = "models/GTA_SA/coupes_hatchbacks/cadrona.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Coupes & Hatchbacks",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 1200,
+		
+		EnginePos = Vector(60,0,10),
+		
+		LightsTable = "cadrona",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/GTA_SA/coupes_hatchbacks/cadrona_wheel.mdl",
+		CustomWheelPosFL = Vector(57.6,32.4,-12.6),
+		CustomWheelPosFR = Vector(57.6,-32.4,-12.6),
+		CustomWheelPosRL = Vector(-47.8,32.4,-12.6),
+		CustomWheelPosRR = Vector(-47.8,-32.4,-12.6),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(4,0,4),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(-5,-14,18),
+		SeatPitch = -10,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(4,-14,-14),
+				ang = Angle(0,-90,14)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-83.1,-16.9,-13.6),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 6,
+		FrontConstant = 36000,
+		FrontDamping = 1300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 5,
+		RearConstant = 36000,
+		RearDamping = 1300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 50,
+		Efficiency = 1,
+		GripOffset = 1,
+		BrakePower = 34,
+		
+		IdleRPM = 800,
+		LimitRPM = 5300,
+		PeakTorque = 150,
+		PowerbandStart = 900,
+		PowerbandEnd = 4900,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-60.8,37,5.2),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 60,
+		
+		PowerBias = -1,
+		
+		EngineSoundPreset = 0,
+
+		Sound_Idle = "bank_001/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_001/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_001/sound_001.wav",
+		Sound_HighPitch = 1.1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_002/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.58,
+		Gears = {-0.12,0,0.1,0.17,0.25,0.34,0.45}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_cadrona", V )
