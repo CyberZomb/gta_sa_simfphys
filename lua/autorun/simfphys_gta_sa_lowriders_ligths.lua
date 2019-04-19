f88be62b26a46a029ef741ff7c9027b328ff9108
def-valerius <31399259+def-valerius@users.noreply.github.com>
2019-04-19 21:09:13 +0500

    Entity
    Lowrider: Slamvan

--- lua/autorun/simfphys_gta_sa_lowriders_ligths.lua
+++ lua/autorun/simfphys_gta_sa_lowriders_ligths.lua
@@ -199,3 +199,52 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "savanna", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 81.62324, 33.358392,-3.053592),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 81.62324,-33.358392,-3.053592),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-84.18932, 33.960528,-5.64624),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-84.18932,-33.960528,-5.64624),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 91.62324, 33.358392,-3.053592),material = "sprites/light_ignorez",size = 37, color = Color( 255,230,230,100)},
+		{pos = Vector( 91.62324,-33.358392,-3.053592),material = "sprites/light_ignorez",size = 37, color = Color( 255,230,230,100)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 91.62324, 26.360856,-3.053592),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 91.62324,-26.360856,-3.053592),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-94.18932, 33.960528,-5.64624),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-94.18932,-33.960528,-5.64624),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-94.18932, 33.960528,-0.518976),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-94.18932,-33.960528,-0.518976),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-94.18932, 33.960528,-0.518976),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-94.18932,-33.960528,-0.518976),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-94.18932, 33.960528,-0.518976),
+			Vector( 90.18936, 29.684664,-7.320204),
+		},
+		Right = {
+			Vector(-94.18932,-33.960528,-0.518976),
+			Vector( 90.18936,-29.684664,-7.320204),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "slamvan", light_table)
--- lua/autorun/simfphys_gta_sa_lowriders_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_lowriders_spawnlist.lua
@@ -316,7 +316,7 @@ local V = {
 		
 		IdleRPM = 800,
 		LimitRPM = 7400,
-		PeakTorque = 200,
+		PeakTorque = 210,
 		PowerbandStart = 900,
 		PowerbandEnd = 7000,
 		Turbocharged = false,
@@ -478,3 +478,119 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_savanna", V )
+
+local V = {
+	Name = "Slamvan",
+	Model = "models/GTA_SA/lowriders/slamvan.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Lowriders",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 1950,
+		
+		EnginePos = Vector(60,0,10),
+		
+		LightsTable = "slamvan",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 5,
+		
+		CustomWheelModel = "models/GTA_SA/lowriders/slamvan_wheel.mdl",
+		CustomWheelPosFL = Vector( 57.08448, 35.028,-14),
+		CustomWheelPosFR = Vector( 57.08448,-35.028,-14),
+		CustomWheelPosRL = Vector(-58.26996, 35.028,-14),
+		CustomWheelPosRR = Vector(-58.26996,-35.028,-14),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(15,0,4),
+		
+		CustomSteerAngle = 45,
+		
+		SeatOffset = Vector(-3,-15,17),
+		SeatPitch = -5,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(9,-16,-16),
+				ang = Angle(0,-90,25)
+			}
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-97.9542,-12.55788,-19.97496),
+				ang = Angle(90,180,0),
+			},
+			{
+				pos = Vector(-97.9542, 12.55788,-19.97496),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 6,
+		FrontConstant = 45000,
+		FrontDamping = 1300,
+		FrontRelativeDamping = 1300,
+		
+		RearHeight = 5,
+		RearConstant = 45000,
+		RearDamping = 1300,
+		RearRelativeDamping = 1300,
+		
+		FastSteeringAngle = 25,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 5,
+		
+		MaxGrip = 65,
+		Efficiency = 1,
+		GripOffset = 2,
+		BrakePower = 34,
+		
+		IdleRPM = 800,
+		LimitRPM = 7400,
+		PeakTorque = 240,
+		PowerbandStart = 900,
+		PowerbandEnd = 7000,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-20.167776,39.37752,9.705816),
+		FuelType = FUELTYPE_PETROL,
+		FuelTankSize = 54,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		
+		Sound_Idle = "bank_069/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_069/sound_001.wav",
+		Sound_MidPitch = 1,
+		Sound_MidVolume = 1,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_069/sound_001.wav",
+		Sound_HighPitch = 0.9,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_069/sound_001.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.58,
+		Gears = {-0.12,0,0.1,0.17,0.28,0.37,0.45}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_slamvan", V )
