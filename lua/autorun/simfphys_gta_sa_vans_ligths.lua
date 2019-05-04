153748488f82876062dc25f765374dc6f1fd66f2
def-valerius <31399259+def-valerius@users.noreply.github.com>
2019-05-04 13:54:15 +0500

    Entity
    Van: Moonbeam

--- lua/autorun/simfphys_gta_sa_vans_ligths.lua
+++ lua/autorun/simfphys_gta_sa_vans_ligths.lua
@@ -97,3 +97,58 @@ local light_table = {
 
 }
 list.Set( "simfphys_lights", "hotdog", light_table)
+
+local light_table = {
+	L_HeadLampPos = Vector( 76.87592, 28.032912,-7.113132),
+	L_HeadLampAng = Angle(0,0,0),
+	R_HeadLampPos = Vector( 76.87592,-28.032912,-7.113132),
+	R_HeadLampAng = Angle(0,0,0),
+	
+	L_RearLampPos = Vector(-79.7804, 36.34992,-11.73456),
+	L_RearLampAng = Angle(0,180,0),
+	R_RearLampPos = Vector(-79.7804,-36.34992,-11.73456),
+	R_RearLampAng = Angle(0,180,0),
+	
+	Headlight_sprites = {
+		{pos = Vector( 86.87592, 28.032912,-7.113132),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,180)},
+		{pos = Vector( 86.87592,-28.032912,-7.113132),material = "sprites/light_ignorez",size = 38, color = Color( 255,230,230,180)},
+	},
+	Headlamp_sprites = {
+		{pos = Vector( 86.87592, 28.032912,-7.113132),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+		{pos = Vector( 86.87592,-28.032912,-7.113132),material = "sprites/light_ignorez",size = 50, color = Color( 255,235,220,170)},
+	},
+	Rearlight_sprites = {
+		{pos = Vector(-89.7804, 36.34992,-11.73456),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+		{pos = Vector(-89.7804,-36.34992,-11.73456),material = "sprites/light_ignorez",size = 35,color = Color( 255, 0, 0,  150)},
+	},
+	Brakelight_sprites = {
+		{pos = Vector(-89.7804, 36.34992,-6),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-89.7804,-36.34992,-6),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-89.7804, 36.34992,-1),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+		{pos = Vector(-89.7804,-36.34992,-1),material = "sprites/light_ignorez",size = 27,color = Color( 255, 0, 0,  180)},
+	},
+	Reverselight_sprites = {
+		{pos = Vector(-89.7804, 36.34992,-1),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+		{pos = Vector(-89.7804,-36.34992,-1),material = "sprites/light_ignorez",size = 17,color = Color( 255, 255, 255, 250)},
+	},
+	
+	DelayOn = 0.1,
+	DelayOff = 0.1,
+	
+	Turnsignal_sprites = {
+		Left = {
+			Vector(-96.0462, 29.197188,-21.458736),
+			Vector(-89.7804, 36.34992,2),
+			Vector( 86.73696, 34.400844,-9.562788),
+			Vector( 86.73696, 34.400844,-6),
+		},
+		Right = {
+			Vector(-96.0462,-29.197188,-21.458736),
+			Vector(-89.7804,-36.34992,2),
+			Vector( 86.73696,-34.400844,-9.562788),
+			Vector( 86.73696,-34.400844,-6),
+		},
+	}
+
+}
+list.Set( "simfphys_lights", "moonbeam", light_table)
--- lua/autorun/simfphys_gta_sa_vans_spawnlist.lua
+++ lua/autorun/simfphys_gta_sa_vans_spawnlist.lua
@@ -237,3 +237,123 @@ local V = {
 	}
 }
 list.Set( "simfphys_vehicles", "simfphys_gta_sa_hotdog", V )
+
+local V = {
+	Name = "Moonbeam",
+	Model = "models/GTA_SA/vans/moonbeam.mdl",
+	Class = "gmod_sent_vehicle_fphysics_base",
+	Category = "GTA SA Vans",
+	SpawnOffset = Vector(0,0,20),
+	SpawnAngleOffset = 90,
+
+	Members = {
+		Mass = 2000,
+		
+		EnginePos = Vector(90,0,10),
+		
+		LightsTable = "moonbeam",
+		
+		CustomWheels = true,
+		CustomSuspensionTravel = 1,
+		
+		CustomWheelModel = "models/GTA_SA/vans/moonbeam_wheel.mdl",
+		CustomWheelPosFL = Vector( 64.70928, 36.54,-25),
+		CustomWheelPosFR = Vector( 64.70928,-36.54,-25),
+		CustomWheelPosRL = Vector(-61.31664, 36.54,-25),
+		CustomWheelPosRR = Vector(-61.31664,-36.54,-25),
+		CustomWheelAngleOffset = Angle(0,90,0),
+		
+		CustomMassCenter = Vector(4,0,4),
+		
+		CustomSteerAngle = 32,
+		
+		SeatOffset = Vector(13,-17,20),
+		SeatPitch = 8,
+		SeatYaw = 90,
+		
+		PassengerSeats = {
+			{
+				pos = Vector(20,-17,-17),
+				ang = Angle(0,-90,8)
+			},
+			{
+				pos = Vector(-26,18,-15),
+				ang = Angle(0,-90,12)
+			},
+			{
+				pos = Vector(-26,-18,-15),
+				ang = Angle(0,-90,12)
+			}
+		
+		},
+		
+		ExhaustPositions = {
+			{
+				pos = Vector(-94.887,-20.478672,-29.7),
+				ang = Angle(90,180,0),
+			}
+		},
+		
+		FrontHeight = 6,
+		FrontConstant = 50000,
+		FrontDamping = 3000,
+		FrontRelativeDamping = 3000,
+		
+		RearHeight = 6,
+		RearConstant = 50000,
+		RearDamping = 3000,
+		RearRelativeDamping = 3000,
+		
+		FastSteeringAngle = 19,
+		SteeringFadeFastSpeed = 535,
+		
+		TurnSpeed = 2,
+		
+		MaxGrip = 70,
+		Efficiency = 1,
+		GripOffset = 0,
+		BrakePower = 50,
+		
+		IdleRPM = 600,
+		LimitRPM = 4000,
+		PeakTorque = 280,
+		PowerbandStart = 700,
+		PowerbandEnd = 3800,
+		Turbocharged = false,
+		Supercharged = false,
+		
+		FuelFillPos = Vector(-63.84096,41.46336,-0.8271),
+		FuelType = FUELTYPE_DIESEL,
+		FuelTankSize = 60,
+		
+		PowerBias = 1,
+		
+		EngineSoundPreset = 0,
+
+		Sound_Idle = "bank_088/sound_002.wav",
+		Sound_IdlePitch = 1,
+		
+		Sound_Mid = "bank_088/sound_001.wav",
+		Sound_MidPitch = 1.5,
+		Sound_MidVolume = 2,
+		Sound_MidFadeOutRPMpercent = 80,
+		Sound_MidFadeOutRate = 0.8,
+		
+		Sound_High = "bank_088/sound_001.wav",
+		Sound_HighPitch = 1.1,
+		Sound_HighVolume = 1.5,
+		Sound_HighFadeInRPMpercent = 80,
+		Sound_HighFadeInRate = 0.8,
+		
+		Sound_Throttle = "bank_089/sound_002.wav",
+		Sound_ThrottlePitch = 1,
+		Sound_ThrottleVolume = 4,
+		
+		--
+		snd_horn = "bank_068/sound_002.wav",
+		
+		DifferentialGear = 0.5,
+		Gears = {-0.12,0,0.14,0.24,0.36,0.48,0.6}
+	}
+}
+list.Set( "simfphys_vehicles", "simfphys_gta_sa_moonbeam", V )
